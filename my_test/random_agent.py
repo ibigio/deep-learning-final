4b87f4012f5b8cdf8c8307d06550129f80d5337f
Ilan Bigio <ilanbigio@gmail.com>
2019-12-03 17:49:46 -0500

    Increased indentation and added some documentation.

--- my_test/random_agent.py
+++ my_test/random_agent.py
@@ -2,11 +2,11 @@ import numpy as np
 
 class RandomAgent:
 
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def step(self, action_history, observations):
-    cur_player = int(observations['current_player'])
-    actions = observations['legal_actions'][cur_player]
-    return np.random.choice(actions)
+    def step(self, action_history, observations):
+        cur_player = int(observations['current_player'])
+        actions = observations['legal_actions'][cur_player]
+        return np.random.choice(actions)
 
